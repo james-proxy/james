0838d3f9c508f09b859cc28e888b6d9236c6c5a4
Julian Hollmann <julian.hollmann@gmail.com>
2015-07-01 19:28:22 +0200

    Remove current labels

--- src/config.js
+++ src/config.js
@@ -2,14 +2,6 @@ export default {
   maxLogEntries: 1000,
 
   labels: [
-    {
-      regex: /common-secure-static/,
-      name: 'CDN',
-      className: 'cdn'
-    }, {
-      regex: /prosieben/,
-      name: 'p7',
-      className: 'p7'
-    },
+
   ]
 }
