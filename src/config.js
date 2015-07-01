d808e170dc1b5c8fb0fcee088912f926a66badfa
Julian Hollmann <julian.hollmann@gmail.com>
2015-07-01 14:46:02 +0200

    Optimize config settings

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
-  maxLogEntries: 300,
+  maxLogEntries: 1000,
 
   labels: [
     {
       regex: /common-secure-static/,
       name: 'CDN',
       className: 'cdn'
-    },{
+    }, {
       regex: /prosieben/,
       name: 'p7',
       className: 'p7'
