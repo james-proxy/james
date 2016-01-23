ce30c7e8f86f260f0ca4a54f106672f95bc180ca
Mitchell Hentges <mitchhentges@protonmail.com>
2016-01-23 13:38:48 +0100

    Resolve lint errors, add todo for handling browser launch failure

--- src/util/open-browser.js
+++ src/util/open-browser.js
@@ -2,18 +2,10 @@ const browserLauncher = require('browser-launcher2');
 export default function openBrowser(browser = 'firefox') {
   const opts = {
     browser: browser,
-    proxy: "localhost:1338"
+    proxy: 'localhost:1338'
   };
 
-  browserLauncher(function(err, launch) {
-    if (err) {
-      return console.error(err);
-    }
-
-    launch('http://www.uxebu.com/', opts, function(err) {
-      if (err) {
-        console.error(err);
-      }
-    })
-  })
+  browserLauncher(function(err, launch) { //TODO handle error, #37
+    launch('http://www.uxebu.com/', opts);
+  });
 }
