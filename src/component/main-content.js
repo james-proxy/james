e305bd639a8d85c99950134e9f0401783526a6c9
Mitchell Hentges <mitchhentges@protonmail.com>
2016-01-24 18:02:08 +0100

    Development

--- src/component/main-content.js
+++ src/component/main-content.js
@@ -60,7 +60,7 @@ export default class MainContent extends React.Component {
     } else {
       const browserElements = browsers.map((browser) => {
         const src = './images/' + browser.name + '_128x128.png';
-        return <img className="open-browser" src={src} key={browser.name}
+        return <img className="open-browser" src={src} key={browser.name} alt={browser.name} title={browser.name}
                     onClick={this._openBrowserFactory(browser.name)} />
       });
 
