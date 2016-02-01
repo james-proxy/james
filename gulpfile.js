38d1b0f8219815a44ecff418a2014fbe4308d374
Mitchell Hentges <mitch9654@gmail.com>
2016-02-01 13:39:07 +0100

    Only run useref on html files

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const source = require('vinyl-source-stream');
 const electron = require('electron-packager');
 const useref = require('gulp-useref');
 const electronConnect = require('electron-connect').server.create();
+Only const gulpif = require('gulp-if');
 const exec = require('child_process').exec;
 
 gulp.task('default', ['js', 'css', 'resources']);
@@ -54,8 +55,10 @@ gulp.task('resources', () => {
 gulp.task('package-resources', ['default'], () => {
   return es.merge([
     gulp.src('node_modules/font-awesome/fonts/**').pipe(gulp.dest('package/fonts')),
-    gulp.src('resource/**').pipe(useref({noAssets: true})).pipe(gulp.dest('package')),
-    gulp.src('build/james.css').pipe(gulp.dest('package'))
+    gulp.src('build/james.css').pipe(gulp.dest('package')),
+    gulp.src('resource/**')
+      .pipe(gulpif('*.html', useref()))
+      .pipe(gulp.dest('package'))
   ]);
 });
 
