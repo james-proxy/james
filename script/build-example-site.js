d33129621ad98765b40cf898d5aeb95a68d99060
Julian Hollmann <julian.hollmann@googlemail.com>
2016-02-05 00:15:47 +0100

    Use proper path, add logging in error case and use rimraf to remove the folder

--- script/build-example-site.js
+++ script/build-example-site.js
@@ -1,18 +1,22 @@
 var marked = require('marked');
 var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+
+var basePath = path.resolve(__dirname, '../') + '/';
 
 try {
-  fs.rmdirSync('./resource/example-site');
-  fs.mkdirSync('./resource/example-site');
+  rimraf.sync(basePath + 'resource/example-site');
+  fs.mkdirSync(basePath + 'resource/example-site');
 } catch(e) {
-  // don't care :)
+  console.log(e);
 }
 
-var template = fs.readFileSync('./resource/example-site.template.html', {encoding: 'utf-8'});
-var readmeText = fs.readFileSync('./README.md', {encoding: 'utf-8'});
+var template = fs.readFileSync(basePath + 'resource/example-site.template.html', {encoding: 'utf-8'});
+var readmeText = fs.readFileSync(basePath + 'README.md', {encoding: 'utf-8'});
 
 var exampleSiteHtml = template
   .replace('<!-- REPLACE -->', marked(readmeText))
   .replace('src="resource', 'src="https://raw.githubusercontent.com/james-proxy/james/master/resource');
 
-fs.writeFileSync('./resource/example-site/index.html', exampleSiteHtml);
+fs.writeFileSync(basePath + 'resource/example-site/index.html', exampleSiteHtml);
