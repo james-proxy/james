aa5967e3ecd3074a11a4073a5b91a9451c264002
Mitchell Hentges <mitch9654@gmail.com>
2016-02-14 23:12:04 +0100

    Fix CR comments

--- src/component/footer/request-count.js
+++ src/component/footer/request-count.js
@@ -5,10 +5,10 @@ const {object, func} = React.PropTypes;
 const RequestCount = ({requestData, clearRequests}) => {
   const {filteredCount, totalCount} = requestData;
   return <div className="request-count">
-    {`Requests: ${filteredCount} / ${totalCount}`}
     <button title="Clear all requests" onClick={clearRequests}>
       <i className="fa fa-ban" />
     </button>
+    {`Requests: ${filteredCount} / ${totalCount}`}
   </div>;
 };
 
