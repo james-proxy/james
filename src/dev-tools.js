f39005b95d68ab73724c7c0dd0acf0f2631ff0e6
Mitchell Hentges <mitchhentges@protonmail.com>
2016-03-14 20:36:34 +0100

    Fix dev tools not opening (XFCE-only? Haven't seen this before)

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -3,15 +3,16 @@ import remote from 'remote';
 export default class DevTools {
   constructor() {
     this._open = false;
-    this._window = remote.getCurrentWindow();
   }
 
   toggle() {
     if (!this._open) {
-      this._window.openDevTools({detach: true});
+      const window = remote.getCurrentWindow();
+      window.openDevTools({detach: true});
       this._open = true;
     } else {
-      this._window.closeDevTools();
+      const window = remote.getCurrentWindow();
+      window.closeDevTools();
       this._open = false;
     }
   }
