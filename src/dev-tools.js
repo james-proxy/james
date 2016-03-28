ee6f3a480753fd774a2dbdcb6dc970e0c5f0f9e3
Mitchell Hentges <mitchhentges@protonmail.com>
2016-03-28 17:47:13 +0200

    Fix "object of undefined" exception with electron-connect Refactors dev-tools back, since ^ is fixed

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -2,17 +2,16 @@ import remote from 'remote';
 
 export default class DevTools {
   constructor() {
+    this._window = remote.getCurrentWindow();
     this._open = false;
   }
 
   toggle() {
     if (!this._open) {
-      const window = remote.getCurrentWindow();
-      window.openDevTools({detach: true});
+      this._window.openDevTools({detach: true});
       this._open = true;
     } else {
-      const window = remote.getCurrentWindow();
-      window.closeDevTools();
+      this._window.closeDevTools();
       this._open = false;
     }
   }
