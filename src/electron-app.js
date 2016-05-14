a133bf5b5355694075177ea41473e21331cadbb4
Mitchell Hentges <mitchhentges@protonmail.com>
2016-05-14 21:35:45 +0200

    Fix lint errors

--- src/electron-app.js
+++ src/electron-app.js
@@ -27,7 +27,7 @@ app.on('ready', () => {
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
