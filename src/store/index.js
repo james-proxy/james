c4ded04fa48ebf11a707319c982ad6191bc64790
Tomi M <tomitm@gmail.com>
2016-06-03 22:26:28 -0700

    Skip logging in prod.

--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,27 @@ import { routerMiddleware } from 'react-router-redux';
 
 import urlMapperMiddleware from './middleware/url-mapper.js';
 import proxyMiddleware from './middleware/proxy.js';
-
 import rootReducer from '../reducers/root.js';
+import constants from '../constants.js';
 
 export default (proxy, urlMapper, history) => {
   const loggerMiddleware = createLogger();
   const routingMiddleware = routerMiddleware(history);
+
+  let middleware = [
+    thunkMiddleware,
+    urlMapperMiddleware(urlMapper),
+    proxyMiddleware(proxy),
+    routingMiddleware
+  ];
+
+  if (constants.DEV) {
+    middleware.push(loggerMiddleware);
+  }
+
   const store = createStore(
     rootReducer,
-    applyMiddleware(
-      thunkMiddleware,
-      urlMapperMiddleware(urlMapper),
-      proxyMiddleware(proxy),
-      routingMiddleware,
-      loggerMiddleware
-    )
+    applyMiddleware(...middleware)
   );
 
 
