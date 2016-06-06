78d1adfde884bd5727a7c3efcc78ab18135e12a2
Tomi M <tomitm@gmail.com>
2016-06-05 23:39:14 -0700

    Maintain scroll position when list updates.

--- src/component/requests/requests.js
+++ src/component/requests/requests.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
 import Request from './request.js';
 
 const {object, array} = React.PropTypes;
 
-const Requests = ({requestData, activeRequest, contextRequest, labels}) => {
-  const requestNodes = requestData.requests.map(({request, response}) => {
-    const isActive = activeRequest && activeRequest.id === request.id || false;
-    const isContextMenu = contextRequest && contextRequest === request.id || false;
+class Requests extends Component {
+  componentWillUpdate() {
+    // remember scroll position for when list changes
+    const node = findDOMNode(this);
+    this.scrollHeight = node.scrollHeight;
+    this.scrollTop = node.scrollTop;
+  }
 
-    return <Request
-      request={request}
-      response={response}
-      done={request.done}
-      isActive={isActive}
-      isContextMenu={isContextMenu}
-      labels={labels}
-      key={request.id}
-    />;
-  });
+  componentDidUpdate() {
+    // restore scroll position after list changes
+    const node = findDOMNode(this);
+    node.scrollTop = this.scrollTop + (node.scrollHeight - this.scrollHeight);
+  }
 
-  return <div className="requests">
-    {requestNodes}
-  </div>;
-};
+  render() {
+    const {requestData, activeRequest, contextRequest, labels} = this.props;
+
+    const requestNodes = requestData.requests.map(({request, response}) => {
+      const isActive = activeRequest && activeRequest.id === request.id || false;
+      const isContextMenu = contextRequest && contextRequest === request.id || false;
+
+      return <Request
+        request={request}
+        response={response}
+        done={request.done}
+        isActive={isActive}
+        isContextMenu={isContextMenu}
+        labels={labels}
+        key={request.id}
+      />;
+    });
+
+    return <div className="requests">
+      {requestNodes}
+    </div>;
+  }
+}
 
 Requests.propTypes = {
   requestData: object.isRequired,
