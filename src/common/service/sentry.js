b82e432bba0f1dd597e8be20a600180413db1389
Mitchell Hentges <mitch9654@gmail.com>
2019-02-02 23:27:31 -0800

    Releases v2.1.2

--- src/common/service/sentry.js
+++ src/common/service/sentry.js
@@ -1,7 +1,7 @@
 import config from '../config.js';
 import constants from '../constants.js';
 
-export default function init(app, Sentry) {
+export default function init(app, Sentry, defaultIntegrations) {
   if (constants.DEV) {
     return;
   }
